b60ea973ddffc9b93a32679beb61d616decb0455
Stefan Stefanov <35034980+s-stefanov@users.noreply.github.com>
2025-07-24 18:57:32 +0300

    feat: create PR checks (#16)
    * feat: create PR checks
    
    * Remove preparation files

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     globals: true,
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "json-summary", "html"],
       include: ["src/core/**/*.ts"],
       exclude: ["src/core/**/*.test.ts", "src/core/types/domain.ts"],
     },
